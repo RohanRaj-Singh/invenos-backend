c494c6d5c69c5685aaba5c61143c3e8817afd8c9
RohanRaj-Singh <mrsinghdev13@gmail.com>
2026-07-30 17:19:34 +0500

    feat(contact): add redirect route for adding new contacts

--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,7 @@ Route::prefix('expenses')->group(function () {
 Route::prefix('contacts')->group(function () {
     Route::get('/', [ContactController::class, 'index'])->name('contacts.index');
     Route::get('/create', [ContactController::class, 'create'])->name('contacts.create');
+    Route::get("/add", function() { return redirect("/contacts/create"); });
     Route::post('/', [ContactController::class, 'store'])->name('contacts.store');
     Route::get('/{id}', [ContactController::class, 'show'])->name('contacts.show');
     Route::put('/{id}', [ContactController::class, 'update'])->name('contacts.update');
